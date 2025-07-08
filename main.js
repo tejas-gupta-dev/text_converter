e5719475a3b3d3db2e69e5a508de3891be956845
tejas-gupta-dev <tejasgupta1512@gmail.com>
2025-07-08 19:55:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const intext = document.getElementById("textinput");
+/*const intext = document.getElementById("textinput");
 const result = document.getElementById("submit");
 result.addEventListener("click", () => {
     const speechsynth = window.speechSynthesis;
@@ -13,4 +13,30 @@ result.addEventListener("click", () => {
         speechsynth.speak(newutter);
         result.textContent = "Sound is play........";
     }
+});*/
+
+const intext = document.getElementById("textinput");
+const result = document.getElementById("submit");
+
+result.addEventListener("click", () => {
+    const speechsynth = window.speechSynthesis;
+    const err = document.querySelector(".error");
+    const tex = intext.value;
+
+    if (!speechsynth.speaking && !tex.trim().length) {
+        err.textContent = "Text missing";
+        return;
+    }
+
+    if (!speechsynth.speaking && tex.trim().length) {
+        err.textContent = "";
+        const newutter = new SpeechSynthesisUtterance(tex);
+        result.textContent = "Speaking...";
+
+        newutter.onend = () => {
+            result.textContent = "Speak";
+        };
+
+        speechsynth.speak(newutter);
+    }
 });
